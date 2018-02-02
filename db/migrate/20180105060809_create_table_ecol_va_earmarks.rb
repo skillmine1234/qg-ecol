db1f2e6c90a570b004d98403719a7176e068d707
Neha1993 <neha.upadhyay@quantiguous.com>
2018-02-02 11:30:38 +0530

    fixed column length

--- db/migrate/20180105060809_create_table_ecol_va_earmarks.rb
+++ db/migrate/20180105060809_create_table_ecol_va_earmarks.rb
@@ -3,7 +3,7 @@ class CreateTableEcolVaEarmarks < ActiveRecord::Migration
     create_table :ecol_va_earmarks, {:sequence_start_value => '1 cache 20 order increment by 1'} do |t|
       t.string :account_no, :limit => 64, :null => false , :comment => "the virtual account no in E-Collect"
       t.string :customer_code, :limit => 20, :null => false, :comment => "the customer code in E-Collect"
-      t.string :hold_no, :limit => 120, :null => false, :comment => "the reference number which is used to identify the hold that is to be released or updated"
+      t.string :hold_no, :limit => 64, :null => false, :comment => "the unique number which has been used to put the hold on virtual account no"
       t.number :hold_amount, :null => false, :default => 0, :comment => "the amount that is held, this is set or updated when a hold is placed"
       t.string :hold_desc, :limit => 255, :null => false, :comment => "the description which consists the purpose of hold"
       t.datetime :created_at, :null => false, :comment => "the timestamp at which the hold is created"
