cf9fc5c087cce88046fd46c90ef5dcc237ce9b26
Neha1993 <neha.upadhyay@quantiguous.com>
2018-02-05 14:57:30 +0530

    sy-395: modified ecol_va_transfers

--- db/migrate/20180205085349_add_app_id_to_ecol_va_transfers.rb
+++ db/migrate/20180205085349_add_app_id_to_ecol_va_transfers.rb
@@ -1,5 +1,6 @@
 class AddAppIdToEcolVaTransfers < ActiveRecord::Migration
   def change
     add_column :ecol_va_transfers, :app_id, :string, :limit => 32, :comment => 'the identifier for the client to process a payment'    
+    change_column :ecol_va_transfers, :rmtr_bene_note, :string, null: true
   end
 end
