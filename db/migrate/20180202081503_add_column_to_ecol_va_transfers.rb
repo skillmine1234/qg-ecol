ee2b23a74310f94153e59f2a9709154e1b930c0b
Neha1993 <neha.upadhyay@quantiguous.com>
2018-02-05 10:10:30 +0530

    sy-395: incorporated review comments

--- db/migrate/20180202081503_add_column_to_ecol_va_transfers.rb
+++ db/migrate/20180202081503_add_column_to_ecol_va_transfers.rb
@@ -1,6 +1,6 @@
 class AddColumnToEcolVaTransfers < ActiveRecord::Migration
   def change
-    add_column :ecol_va_transfers, :ecol_va_txn_id, :int, comment: 'the id of ecol_va_txns tale which is required to show the transaction detail'                 
+    add_column :ecol_va_transfers, :ecol_va_txn_id, :int, comment: 'the id of ecol_va_txns table which is required to show the transaction detail'
     add_column :ecol_va_transfers, :customer_id, :string, limit: 50, comment: 'the customer id of an ecollect customer'       
     add_column :ecol_va_transfers, :fault_code, :string, :limit => 50, :comment => 'the code that identifies the exception, if an exception occured in the ESB'
     add_column :ecol_va_transfers, :fault_subcode, :string, :limit => 50, :comment => "the error code that the third party will return"
