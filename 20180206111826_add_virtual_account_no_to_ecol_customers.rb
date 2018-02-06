d8a494be5e7a5ca7e33f0ec3c03a4ed836e31584
Neha1993 <neha.upadhyay@quantiguous.com>
2018-02-06 23:27:54 +0530

    sy-395: added required columns in ecol_transactions table

--- 20180206111826_add_virtual_account_no_to_ecol_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddVirtualAccountNoToEcolCustomers < ActiveRecord::Migration
-  def change
-    add_column :ecol_transactions, :virtual_account_no, :string, limit: 64, :comment => "the virtual account no of an ecollect customer"    
-  end
-end
