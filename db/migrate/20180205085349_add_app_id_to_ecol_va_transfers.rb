bd886cfe9f9bc634f708099eb87932448a69bfce
Neha1993 <neha.upadhyay@quantiguous.com>
2018-02-06 12:39:03 +0530

    sy-395: table are modified

--- db/migrate/20180205085349_add_app_id_to_ecol_va_transfers.rb
+++ db/migrate/20180205085349_add_app_id_to_ecol_va_transfers.rb
@@ -1,9 +1,10 @@
 class AddAppIdToEcolVaTransfers < ActiveRecord::Migration
   def change
-    add_column :ecol_va_transfers, :app_id, :string, :limit => 32, :comment => 'the identifier for the client to process a payment'    
-    change_column :ecol_va_transfers, :rmtr_bene_note, :string, null: true
-    remove_column :ecol_va_transfers, :cbs_ref_no, :string
-    remove_column :ecol_va_transfers, :confirmed_at, :datetime
-    change_column :ecol_va_transfers, :transfer_type, :string, null: true
+    #add_column :ecol_va_transfers, :app_id, :string, :limit => 32, :comment => 'the identifier for the client to process a payment'    
+    #change_column :ecol_va_transfers, :rmtr_bene_note, :string, null: true
+    #remove_column :ecol_va_transfers, :cbs_ref_no, :string
+    #remove_column :ecol_va_transfers, :confirmed_at, :datetime
+    #change_column :ecol_va_transfers, :transfer_type, :string, null: true
+    change_column :ecol_va_transfers, :status_code, :string, :limit => 32
   end
 end
