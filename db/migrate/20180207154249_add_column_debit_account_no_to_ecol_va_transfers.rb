8621f3beb58ff7c609152220c2af0db3186fcfb7
naveen <naveen.pokuri@quantiguous>
2018-02-09 11:57:56 +0530

    new columns for ecol-va

---
+++ db/migrate/20180207154249_add_column_debit_account_no_to_ecol_va_transfers.rb
@@ -0,0 +1,12 @@
+class AddColumnDebitAccountNoToEcolVaTransfers < ActiveRecord::Migration
+  def change
+    add_column :ecol_va_transfers, :debit_account_no, :string, limit: 20, null: false, comment: "the account to be debited"    
+  end
+end
+
+
+
+
+
+
+
