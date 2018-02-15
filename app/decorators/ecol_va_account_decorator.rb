2365f335241dc045c120e2554d32c80228da4b6c
mahesh32 <mahesh.babu@quantiguous.com>
2018-02-15 18:42:03 +0530

    GEM-49,GEM-52,GEM-51:ECollect VA Transfers Advanced search filed Transfer Type and status code should have drop down options

--- app/decorators/ecol_va_account_decorator.rb
+++ app/decorators/ecol_va_account_decorator.rb
@@ -1,3 +1,9 @@
 class EcolVaAccountDecorator < EcolOperationDecorator
-
+  def hold_amount
+    h.number_to_currency(object.hold_amount, unit: 'Rs ')
+  end
+  
+  def account_balance
+    h.number_to_currency(object.account_balance, unit: 'Rs ')
+  end
 end
