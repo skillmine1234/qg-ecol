d1c8d027654902fe7f7466fcf752c62cceec5b70
QG <qg@QGs-MacBook-Pro-4.local>
2018-03-06 18:06:52 +0530

    SY-386 column length changes on ecol_transactions

--- db/migrate/20180306115926_change_length_of_ecol_transactions.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLengthOfEcolTransactions < ActiveRecord::Migration
-  def change
-   change_column :ecol_transactions, :rmtr_sms_notify_ref, :string, limit: 2000
-  end
-end
---
+++ db/migrate/20180306123131_change_column_length_of_ecol_transactions.rb
@@ -0,0 +1,5 @@
+class ChangeColumnLengthOfEcolTransactions < ActiveRecord::Migration
+  def change
+  change_column :ecol_transactions, :rmtr_sms_notify_ref, :string, limit: 2000 
+  end
+end
