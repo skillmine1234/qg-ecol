5a4563c8acfb8864a745ccd12b41f281660fbaac
pravinkarale <pravinkarale21@gmail.com>
2019-10-10 16:43:22 +0530

    call new procedure for override validation in pending validation

--- app/models/ecol_transaction.rb
+++ app/models/ecol_transaction.rb
@@ -39,7 +39,11 @@ class EcolTransaction < ActiveRecord::Base
   end
   
   def override(status, user_id, remarks)
-    result = plsql.pk_qg_ecol_audit_helper.override_and_enqueue(id, status, user_id, nil, remarks, nil, nil)
+    if self.status == 'VALIDATION FAILED'
+      result = plsql.pk_qg_ecol_audit_helper.override_and_enqueue(id, status, user_id, nil, remarks, nil, nil)
+    elsif self.status == 'PENDING VALIDATION'
+      result = plsql.pk_qg_ecol_audit_helper.ecollect_pendingValidation(id, status, user_id, nil, remarks, nil, nil)
+    end
     raise ::Fault::ProcedureFault.new(OpenStruct.new(code: result[:po_fault_code], subCode: nil, reasonText: "#{result[:po_fault_reason]}")) if result[:po_fault_code].present?
   end
 end
--- lib/qg/ecol/version.rb
+++ lib/qg/ecol/version.rb
@@ -1,5 +1,5 @@
 module Qg
   module Ecol
-    VERSION = "1.9.0"
+    VERSION = "2.0.0"
   end
 end
