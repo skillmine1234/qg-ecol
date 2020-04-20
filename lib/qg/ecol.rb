30e51f05b11695e37897c8899c62cc9ba4d5e612
pravinkarale <pravinkarale21@gmail.com>
2020-04-20 10:17:25 +0530

    menu removal

--- lib/qg/ecol.rb
+++ lib/qg/ecol.rb
@@ -4,14 +4,14 @@ module Qg
   module Ecol
     NAME = 'ECollect'
     GROUP = 'e-collect'
-    MENU_ITEMS = [:ecol_app, :ecol_customer, :ecol_remitter, :ecol_transaction, :udf_attribute, :ecol_summary, :ecol_va_memo_txn]
+    MENU_ITEMS = [:ecol_app, :ecol_customer, :ecol_remitter, :ecol_transaction, :udf_attribute, :ecol_summary]
     MODELS = ['EcolRule','EcolCustomer','EcolRemitter','EcolTransaction','UdfAttribute','IncomingFile','EcolFetchStatistic','QgEcolTodaysNeftTxn',
               'QgEcolTodaysRtgsTxn','QgEcolTodaysImpsTxn','QgEcolTodaysUpiTxn','OutgoingFile','EcolApp','EcolAppUdtable','EcolSummary','EcolVaTxn',
               'EcolVaAccount','EcolVaEarmark','EcolVacdIncomingFile','EcolVacdIncomingRecord','IncomingFileRecord','EcolVaMemoTxn','EcolVaTransfer',
               'EcolIncomingFile','EcolIncomingRecord']
     TEST_MENU_ITEMS = [:qg_ecol_todays_neft_txn, :qg_ecol_todays_rtgs_txn, :qg_ecol_todays_imps_txn, :qg_ecol_todays_upi_txn]
-    COMMON_MENU_ITEMS = [:incoming_file, :outgoing_file]
+    COMMON_MENU_ITEMS = [:incoming_file]
     RULE = :ecol_rule
-    OPERATIONS = [:ecol_va_txn, :ecol_va_transfer, :ecol_va_earmark]
+    OPERATIONS = []
   end
 end
--- lib/qg/ecol/version.rb
+++ lib/qg/ecol/version.rb
@@ -1,5 +1,5 @@
 module Qg
   module Ecol
-    VERSION = "5.1.0"
+    VERSION = "5.2.0"
   end
 end
