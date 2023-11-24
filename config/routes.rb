b844835a5801f34f39736d7906f7b629fcd2e8e6
skillmine1234 <rahul.khoragade@skill-mine.com>
2023-11-24 16:40:11 +0530

    appsec-changes-ecol-new

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   resources :ecol_remitters
   resources :ecol_transactions do
     collection do
-      put  'update_multiple'
+      get  'update_multiple'
     end
     member do
-      put :override_transaction_add_to_approval 
-      put :override_transaction
-      put :pending_validation
-      put :approve_ecol_trans
+      get :override_transaction_add_to_approval 
+      get :override_transaction
+      get :pending_validation
+      get :approve_ecol_trans
     end
   end
   resources :udf_attributes
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :ecol_apps do
     collection do
-      put :index
+      get :index
     end
   end
 
@@ -41,23 +41,23 @@ Rails.application.routes.draw do
   get '/ecol_customer/:id/audit_logs' => 'ecol_customers#audit_logs'
   get '/ecol_remitter/:id/audit_logs' => 'ecol_remitters#audit_logs'
   get '/ecol_error_msg' => "ecol_rules#error_msg"
-  put '/ecol_customer/:id/approve' => "ecol_customers#approve"
-  put '/ecol_remitter/:id/approve' => "ecol_remitters#approve"
-  put '/ecol_rule/:id/approve' => "ecol_rules#approve"
+  get '/ecol_customer/:id/approve' => "ecol_customers#approve"
+  get '/ecol_remitter/:id/approve' => "ecol_remitters#approve"
+  get '/ecol_rule/:id/approve' => "ecol_rules#approve"
   get '/ecol_transactions/:id/ecol_audit_logs/:step_name' => 'ecol_transactions#ecol_audit_logs'
-  put '/ecol_transactions/:id/approve' => "ecol_transactions#approve_transaction"
+  get '/ecol_transactions/:id/approve' => "ecol_transactions#approve_transaction"
   get 'ecol_apps/:app_code/ecol_app_udtables' => 'ecol_apps#ecol_app_udtables', as: :udtables
   get '/ecol_app_udtables/udfs/:app_code' => 'ecol_app_udtables#udfs'
   get '/ecol_app_udtables/:id/audit_logs' => 'ecol_app_udtables#audit_logs'
-  put '/ecol_app_udtables/:id/approve' => "ecol_app_udtables#approve"
+  get '/ecol_app_udtables/:id/approve' => "ecol_app_udtables#approve"
   get '/ecol_apps/:id/audit_logs' => 'ecol_apps#audit_logs'
-  put '/ecol_apps/:id/approve' => "ecol_apps#approve"
+  get '/ecol_apps/:id/approve' => "ecol_apps#approve"
   get '/udf_attribute/:id/audit_logs' => 'udf_attributes#audit_logs'
-  put '/udf_attribute/:id/approve' => "udf_attributes#approve"
+  get '/udf_attribute/:id/approve' => "udf_attributes#approve"
   
   resources :ecol_vacd_incoming_records do
     collection do
-      put :index
+      get :index
     end
   end
   
@@ -66,7 +66,7 @@ Rails.application.routes.draw do
   
   resources :ecol_incoming_records do
     collection do
-      put :index
+      get :index
     end
     member do
       get :audit_logs
--- lib/qg/ecol/version.rb
+++ lib/qg/ecol/version.rb
@@ -1,5 +1,5 @@
 module Qg
   module Ecol
-    VERSION = "13.7.0"
+    VERSION = "13.8.0"
   end
 end
