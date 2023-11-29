30744131764af908ea677d80b390b6b125c4f2fa
pravinkarale <pravinkarale21@gmail.com>
2023-11-29 20:40:19 +0530

    added code for appsec

--- config/routes.rb
+++ config/routes.rb
@@ -50,9 +50,9 @@ Rails.application.routes.draw do
     end
   end
   
-  post '/ecol_rule/:id/audit_logs' => 'ecol_rules#audit_logs'
-  post '/ecol_customer/:id/audit_logs' => 'ecol_customers#audit_logs'
-  post '/ecol_remitter/:id/audit_logs' => 'ecol_remitters#audit_logs'
+  get '/ecol_rule/:id/audit_logs' => 'ecol_rules#audit_logs'
+  get '/ecol_customer/:id/audit_logs' => 'ecol_customers#audit_logs'
+  get '/ecol_remitter/:id/audit_logs' => 'ecol_remitters#audit_logs'
   post '/ecol_error_msg' => "ecol_rules#error_msg"
   post '/ecol_customer/:id/approve' => "ecol_customers#approve"
   post '/ecol_remitter/:id/approve' => "ecol_remitters#approve"
@@ -63,14 +63,14 @@ Rails.application.routes.draw do
   post '/ecol_app_udtables/udfs/:app_code' => 'ecol_app_udtables#udfs'
   post '/ecol_app_udtables/:id/audit_logs' => 'ecol_app_udtables#audit_logs'
   post '/ecol_app_udtables/:id/approve' => "ecol_app_udtables#approve"
-  post '/ecol_apps/:id/audit_logs' => 'ecol_apps#audit_logs'
+  get '/ecol_apps/:id/audit_logs' => 'ecol_apps#audit_logs'
   post '/ecol_apps/:id/approve' => "ecol_apps#approve"
   post '/udf_attribute/:id/audit_logs' => 'udf_attributes#audit_logs'
   post '/udf_attribute/:id/approve' => "udf_attributes#approve"
   
   resources :ecol_vacd_incoming_records do
     collection do
-      post :index
+      patch :index
     end
   end
   
--- lib/qg/ecol/version.rb
+++ lib/qg/ecol/version.rb
@@ -1,5 +1,5 @@
 module Qg
   module Ecol
-    VERSION = "14.1.0"
+    VERSION = "14.2.0"
   end
 end
